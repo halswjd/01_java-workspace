8a36ff4b61c0ba2cb3cd56e3a34c2853fd13512e
mmm2816 <minjung3229@daum.net>
2023-06-13 23:17:51 +0900

    0613

--- 09_API/src/com/kh/practice/token/view/TokenMenu.java
+++ 09_API/src/com/kh/practice/token/view/TokenMenu.java
@@ -5,78 +5,57 @@ import java.util.Scanner;
 import com.kh.practice.token.controller.TokenController;
 
 public class TokenMenu {
-	
+
 	private Scanner sc = new Scanner(System.in);
 	private TokenController tc = new TokenController();
-	
-	
+
 	public void mainMenu() {
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("1. 지정 문자열");
 			System.out.println("2. 입력 문자열");
 			System.out.println("9. 프로그램 끝내기");
-			
+
 			System.out.print("메뉴 번호 : ");
 			int num = sc.nextInt();
-			
+
 			TokenMenu tm = new TokenMenu();
-			
-			if(num == 1 | num == 2) {
-				if(num ==1) {
-					tm.tokenMenu();
-					continue;
-				} else if(num==2) {
-					tm.inputMenu();
-					continue;
-				}
+
+			if (num == 1) {
+				tm.tokenMenu();
+				continue;
+			} else if (num == 2) {
+				tm.inputMenu();
+				continue;
 			}
-			
-			
-			if(num == 9) {
+
+			if (num == 9) {
 				System.out.println("프로그램을 종료합니다");
 				break;
 			}
-			
+
 			System.out.println("잘못입력하셨습니다. 다시 입력하세요.");
 		}
+
 		
-	
-		
-		/*
-		switch(num) {
-		case 1 :
-			tm.tokenMenu();
-			break;
-		case 2 :
-			tm.inputMenu();
-			break;
-		case 9 :
-			System.out.println("프로그램을 종료합니다");
-		}
-		*/
-		
-		
-		
-		
+
 	}
-	
-	public void tokenMenu(){
+
+	public void tokenMenu() {
 //		String str = “J a v a P r o g r a m “; 
 		// 토큰 처리 전 글자, 토큰 처리 전 개수를 출력
 		// TokenController(tc)의 afterToken()의 반환 값을 가지고
 		// 토큰 처리 후 글자, 토큰 처리 후 개수, 모두 대문자로 변환 한 것을 출력
 		System.out.print("토큰 처리 전 글자 : ");
 		String str = sc.nextLine();
-		System.out.println("토큰 처리 전 개수 : " + str.length() );
+		System.out.println("토큰 처리 전 개수 : " + str.length());
 		System.out.println("토큰 처리 후 글자 : " + tc.afterToken(str));
 		System.out.println("토큰 처리 후 개수 : " + tc.afterToken(str).length());
-		
-		System.out.println("모두 대문자로 변환 : " + tc.afterToken(str).toUpperCase());
-		}
 
-	
-	public void inputMenu(){
+		System.out.println("모두 대문자로 변환 : " + tc.afterToken(str).toUpperCase());
+	}
+
+	public void inputMenu() {
 //		문자열을 입력하세요 :
 		// tc에 firstCap()로 입력 받은 문자열을 넘기고 반환 값 출력
 //		찾을 문자를 하나 입력하세요 : 
@@ -88,6 +67,6 @@ public class TokenMenu {
 		System.out.print("찾을 문자 하나를 입력하세요 : ");
 		char ch = sc.nextLine().charAt(0);
 		System.out.println(ch + " 문자가 들어간 개수 : " + tc.findChar(str, ch));
-		}
+	}
 
 }
